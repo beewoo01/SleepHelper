ecc90f5c5ae1c620eb9754bc50a658db9b4521ab
nenene45 <nenene45@naver.com>
2020-09-25 11:11:05 +0900

    20200925

--- app/src/main/java/com/android/sleephelper/MainActivity.java
+++ app/src/main/java/com/android/sleephelper/MainActivity.java
@@ -32,13 +32,14 @@ import java.util.UUID;
 public class MainActivity extends AppCompatActivity {
     Context activity;
 
+    private String ve = "2020-09-24";
     private BluetoothAdapter bluetoothAdapter;
     private Set<BluetoothDevice> devices;
     private BluetoothDevice bluetoothDevice;
     private BluetoothSocket bluetoothSocket = null;
     int pariedDeviceCount;
 
-
+    Thread Sound;
 
 
     final int ALPHA = 1000;
@@ -47,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     FitButton button_alpha, button_beta;
     private boolean isPlaying_alpha = false, isPlaying_beta = false;
     private AudioManager audioManager;
+    Boolean play = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,20 +86,24 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        selectBluetoothDevice();
+        //selectBluetoothDevice();
+
+        Sound = new Thread(){
+            public void run(){
+
+            }
+        };
     }
 
     @Override
     protected void onResume(){
         super.onResume();
         if (bluetoothAdapter == null) {
-            Toast.makeText(this, "블루투스 미지원 기기!",Toast.LENGTH_LONG);
+            Toast.makeText(activity, "블루투스 미지원 기기!",Toast.LENGTH_LONG);
             finish();
         } else {
-            if (bluetoothAdapter.isEnabled()) {
-
-            } else {
-                Toast.makeText(this, "블루투스를 켜주세요!",Toast.LENGTH_LONG);
+            if (!bluetoothAdapter.isEnabled()) {
+                Toast.makeText(activity, "블루투스를 켜주세요!",Toast.LENGTH_LONG);
                 Intent intent = new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
                 startActivity(intent);
             }
@@ -105,15 +111,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void playSound(int type) throws IOException {
+    @Override
+    protected void onStop(){
+        super.onStop();
+        try {
+            if(isPlaying_alpha){
+                playSound(ALPHA);
+                isPlaying_alpha = false;
+            }
+            if(isPlaying_beta) {
+                playSound(BETA);
+                isPlaying_beta = false;
+            }
+            play = false;
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+
+    private void playSound(int type) throws IOException {
        // Log.e("status", "play : " + isPlaying + "  " + type + " : start");
+        play = false;
+
         if (type == ALPHA) {
             if(isPlaying_beta){
                 media_player.stop();
                 isPlaying_beta = false;
             }
-            if(isPlaying_alpha) {
+            else if(isPlaying_alpha) {
                 media_player.stop();
                 isPlaying_alpha = false;
             }else{
@@ -121,17 +147,34 @@ public class MainActivity extends AppCompatActivity {
                 media_player.setLooping(true);
                 isPlaying_alpha = true;
                 media_player.start();
-                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
-                        (int) (audioManager.getStreamMaxVolume(audioManager.STREAM_MUSIC) * 1),
-                        audioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+                new Thread(new Runnable(){
+                    public void run(){
+                        play = true;
+                        double vol = 0.1;
+                        while (play){
+                            if(play) {
+                                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                                        (int) (audioManager.getStreamMaxVolume(audioManager.STREAM_MUSIC) * vol),
+                                        audioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+                                vol = vol + 0.01;
+                            }
+                            if(vol == 1)
+                                play = false;
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
             }
-
         } else if (type == BETA) {
             if(isPlaying_alpha){
                 media_player.stop();
                 isPlaying_alpha = false;
             }
-            if(isPlaying_beta) {
+            else if(isPlaying_beta) {
                 media_player.stop();
                 isPlaying_beta = false;
             }else{
@@ -139,9 +182,27 @@ public class MainActivity extends AppCompatActivity {
                 media_player.setLooping(true);
                 isPlaying_beta = true;
                 media_player.start();
-                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
-                        (int) (audioManager.getStreamMaxVolume(audioManager.STREAM_MUSIC) * 0.5),
-                        audioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+                new Thread(new Runnable(){
+                    public void run(){
+                        play = true;
+                        double vol = 0.1;
+                        while (play){
+                            if(play) {
+                                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                                        (int) (audioManager.getStreamMaxVolume(audioManager.STREAM_MUSIC) * vol),
+                                        audioManager.FLAG_REMOVE_SOUND_AND_VIBRATE);
+                                vol = vol + 0.01;
+                            }
+                            if(vol == 1)
+                                play = false;
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
             }
         }
     }
@@ -211,6 +272,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
+        }).start();
+        new Thread(new Runnable(){
+            public void run(){
+            }
         }).start();
     }
 
